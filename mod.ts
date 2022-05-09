d35d535811f6a37c0cb397a9068f3c3ff9f5b616
Simone <startae14@gmail.com>
2022-05-09 20:44:02 +0200

    Serving Static files with Deno

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import { Application } from "https://deno.land/x/oak@v10.2.1/mod.ts";
+import { Application, send } from "https://deno.land/x/oak@v10.2.1/mod.ts";
 
 // create an instance of the Application class
 const app = new Application();
@@ -17,6 +17,22 @@ app.use(async (ctx, next) => {
     await next();
     const delta = Date.now() - start;
     ctx.response.headers.set("X-Response-Time", `${delta}ms`);
+});
+
+// serving static files
+app.use(async (ctx, next) => {
+    const filePath = ctx.request.url.pathname;
+    const fileWhitelist = [
+        '/index.html',
+        '/javascripts/script.js',
+        '/stylesheets/style.css',
+        '/images/favicon.png',
+    ];
+    if (fileWhitelist.includes(filePath)) {
+        await send(ctx, filePath, {
+            root: `${Deno.cwd()}/public`,
+        });
+    }
 });
 
 // use method of oak
